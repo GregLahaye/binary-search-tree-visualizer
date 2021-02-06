8dc67d5259ff5b8cacc569225f04680f6b6fe149
Greg Lahaye <greg@lahayes.net>
2021-02-06 11:40:57 +0800

    remove animation code

--- index.js
+++ index.js
@@ -22,12 +22,8 @@ class BinarySearchTree {
       throw new Error("Key " + key + " not found");
     }
 
-    this.highlight(currNode.key);
-    await waitAsync(1000);
-
     if (key === currNode.key) {
       value = currNode.key;
-      found(currNode.key);
     } else if (key < currNode.key) {
       value = this.findRec(key, currNode.left);
     } else {
@@ -120,9 +116,9 @@ class SVGBinarySearchTree extends BinarySearchTree {
     const yDelta = dstPoint.y - srcPoint.y;
 
     const height = this.root.height;
-    radius = Math.min(yDelta / 2 ** height, MAX_RADIUS);
+    const radius = Math.min(yDelta / 2 ** height, MAX_RADIUS);
 
-    [, lines, circles, texts] = this.generate(
+    let [, lines, circles, texts] = this.generate(
       this.root,
       radius,
       srcPoint,
@@ -133,26 +129,9 @@ class SVGBinarySearchTree extends BinarySearchTree {
     circles = circles.map((circle) => circle.generate());
     texts = texts.map((text) => text.generate());
 
-    elements = [...lines, ...circles, ...texts];
+    const elements = [...lines, ...circles, ...texts];
     const innerHTML = elements.join("\n");
     document.getElementById("visual").innerHTML = innerHTML;
-
-    tree.find(Math.round(Math.random() * 100));
-  }
-
-  highlight(key) {
-    for (const element of document.getElementsByTagName("circle")) {
-      if (+element.dataset.value === key) {
-        element.style.fill = "coral";
-      }
-    }
-
-    for (const element of document.getElementsByTagName("line")) {
-      if (+element.dataset.dst === key) {
-        element.style.stroke = "coral";
-        element.style.strokeWidth = element.style.strokeWidth * 2;
-      }
-    }
   }
 }
 
@@ -218,88 +197,12 @@ function generateTree(lower, upper) {
   return root;
 }
 
-function found(val) {
-  for (const element of document.getElementsByTagName("circle")) {
-    if (+element.dataset.value === val) {
-      element.style.fill = "lime";
-    }
-  }
-}
-
-async function findNode(val) {
-  let point;
-
-  for (const element of document.getElementsByTagName("circle")) {
-    if (+element.dataset.value === val) {
-      point = new Point(element.getAttribute("cx"), element.getAttribute("cy"));
-    }
-  }
-
-  circles.push(
-    `<circle data-focus="true" cx="${point.x}" cy="${point.y}" r="${
-      radius * 1.2
-    }" stroke="coral" stroke-width="${radius / 3}" fill="none" />`
-  );
-
-  elements = [...lines, ...circles, ...texts];
-  const innerHTML = elements.join("\n");
-  document.getElementById("visual").innerHTML = innerHTML;
-
-  await waitAsync(1000);
-
-  for (const element of document.getElementsByTagName("circle")) {
-    if (Boolean(element.dataset.focus) === true) {
-      element.style.transform = `translate(${10}px, ${10}px)`;
-      element.style.transition = "all 1.5s linear";
-    }
-  }
-}
-
-function deleteNode(srcVal, dstVal) {
-  let srcPoint;
-  let dstPoint;
-
-  for (const element of document.getElementsByTagName("circle")) {
-    if (+element.dataset.value === srcVal) {
-      srcPoint = new Point(
-        element.getAttribute("cx"),
-        element.getAttribute("cy")
-      );
-    } else if (+element.dataset.value === dstVal) {
-      dstPoint = new Point(
-        element.getAttribute("cx"),
-        element.getAttribute("cy")
-      );
-    }
-  }
-
-  const movement = new Point(srcPoint.x - dstPoint.x, srcPoint.y - dstPoint.y);
-
-  const texts = document.getElementsByTagName("text");
-  const circles = document.getElementsByTagName("circle");
-  const lines = document.getElementsByTagName("line");
-  const elements = [...texts, ...circles, ...lines];
-
-  for (const element of elements) {
-    if (+element.dataset.value === srcVal) {
-      element.classList.add("deleted");
-    } else if (+element.dataset.value === dstVal) {
-      element.style.transform = `translate(${movement.x}px, ${movement.y}px)`;
-      element.style.transition = "all 0.5s linear";
-    }
-  }
-}
-
 function waitAsync(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 const TREE_SPAWN_MULTIPLIER = 8;
 const MAX_RADIUS = 5;
-
-let radius;
-let texts, circles, lines;
-let elements;
 
 const root = generateTree(0, 100);
 const tree = new SVGBinarySearchTree(root);
